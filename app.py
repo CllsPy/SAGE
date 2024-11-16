8d07fa2011dfc562e7eb15410004bde04da13915
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:41:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 # Upload the PDF
 #uploaded_file = st.file_uploader("Upload your research paper (PDF)", type="pdf")
-uploaded_file = st.file_uploader(genai.upload_file())
+uploaded_file = st.file_uploader(genai.upload_file(''))
 if uploaded_file is not None:
 
     file = genai.get_file(uploaded_file)
