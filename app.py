78465117386c1c42399d58d36278be3789e1bfd6
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:37:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 import streamlit as st
 from main import summarize
-import fitz  # PyMuPDF
+import google.generativeai as genai
 
 # Upload the PDF
 uploaded_file = st.file_uploader("Upload your research paper (PDF)", type="pdf")
 
-# def extract_text_from_pdf(pdf_file):
-#     """Extracts text from the given PDF."""
-#     with fitz.open(stream=pdf_file, filetype="pdf") as doc:
-#         text = ""
-#         for page in doc:
-#             text += page.get_text()  # Extract text from each page
-#     return text
-
 if uploaded_file is not None:
-    file = google-generativeai.get_file(uploaded_file)
+    file = genai.get_file(uploaded_file)
     summary = summarize(file) 
-    file
+    summary
     
 
