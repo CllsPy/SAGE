60d87798a161a5b66ca5dc70ec8fdfc69d19307f
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 23:45:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,32 @@ import streamlit as st
 # Upload the PDF
 uploaded_file = st.file_uploader('', type='pdf')
 
-if uploaded_file is not None:
-    try:
-        # Read PDF content
-        pdf_reader = PyPDF2.PdfReader(uploaded_file)
-        text_content = ""
-        for page in pdf_reader.pages:
-            text_content += page.extract_text()
-        
-        if text_content.strip():
-            # Get the response from the summarization function
 
-            with st.spinner("Wait..."):
-                time.sleep(5)
-                response = summarize(text_content)
-                st.write("DONE!")
+with st.form('Gemini Paper Summarizer'):
+    if uploaded_file is not None:
+        try:
+            # Read PDF content
+            pdf_reader = PyPDF2.PdfReader(uploaded_file)
+            text_content = ""
+            for page in pdf_reader.pages:
+                text_content += page.extract_text()
             
-            try:
-                    summarized_text = (response.text)
-                    st.write(summarized_text)
-                    
-            except (KeyError, IndexError, AttributeError) as e:
-                st.error(f"Unable to parse the summary response. Check the response format. Error: {e}")
-        else:
-            st.warning("The PDF does not contain readable text.")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+            if text_content.strip():
+                # Get the response from the summarization function
+    
+                with st.spinner("Wait..."):
+                    time.sleep(5)
+                    response = summarize(text_content)
+                    st.write("DONE!")
+                
+                try:
+                        summarized_text = (response.text)
+                        st.write(summarized_text)
+                        
+                except (KeyError, IndexError, AttributeError) as e:
+                    st.error(f"Unable to parse the summary response. Check the response format. Error: {e}")
+            else:
+                st.warning("The PDF does not contain readable text.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
