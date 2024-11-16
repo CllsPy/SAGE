d08e8504c6663bec5ff6fb3939dc036c32e20e24
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:12:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from main import summarize
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Upload your research paper (PDF)", type="pdf")
 if uploaded_file is not None:
   st.markdown((summarize(uploaded_file)))
