20ebadae3e58de568bca7966c7dff853746757eb
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:17:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from main import summarize
 
 uploaded_file = st.file_uploader("Upload your research paper (PDF)", type="pdf")
 if uploaded_file is not None:
-  pdfFile = genai.upload_file(path=pdf_file, name=name, resumable=True)
+  pdfFile = genai.upload_file(path=uploaded_file, name=name, resumable=True)
   st.markdown((summarize(pdfFile)))
