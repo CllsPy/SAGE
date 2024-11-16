9f7a0256af9b78c9dea25c67b89d9c7e34be851f
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:33:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,17 @@ import fitz  # PyMuPDF
 # Upload the PDF
 uploaded_file = st.file_uploader("Upload your research paper (PDF)", type="pdf")
 
-def extract_text_from_pdf(pdf_file):
-    """Extracts text from the given PDF."""
-    with fitz.open(stream=pdf_file, filetype="pdf") as doc:
-        text = ""
-        for page in doc:
-            text += page.get_text()  # Extract text from each page
-    return text
+# def extract_text_from_pdf(pdf_file):
+#     """Extracts text from the given PDF."""
+#     with fitz.open(stream=pdf_file, filetype="pdf") as doc:
+#         text = ""
+#         for page in doc:
+#             text += page.get_text()  # Extract text from each page
+#     return text
 
 if uploaded_file is not None:
-    # Extract text from the PDF
-    pdf_text = extract_text_from_pdf(uploaded_file.read())
+    file = genai.get_file(uploaded_file)
+    summary = summarize(file) 
+    file
+    
 
-    # Pass the extracted text to the summarize function
-    summary = summarize(pdf_text)  # Assuming summarize now expects text
-
-    # Display the summary
-    st.markdown(summary["candidates"][0]["content"]["parts"][0]["text"])
