734ad139d97e6513e96369f4206db332781c1cee
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 02:44:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from main import summarize
 import google.generativeai as genai
 
 # Upload the PDF
-uploaded_file = st.upload_file(type='pdf)
+uploaded_file = st.upload_file(type='pdf')
 if uploaded_file is not None:
     summary = summarize(uploaded_file) 
     summary
