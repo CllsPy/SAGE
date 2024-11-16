0b4317cc5210af107026995bc11f98e58cd6068a
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-16 01:45:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import google.generativeai as genai
 
-sec_key = userdata.get('gem-api')
-genai.configure(api_key=sec_key)
+import os
+from dotenv import load_dotenv
 
+# Carregando as variáveis de ambiente do arquivo .env
+load_dotenv()
+# Acessando a variável de ambiente API_KEY
+api_key = os.getenv("API_KEY")
+# Verificando se a API_KEY foi carregada com sucesso
+if api_key:
+    print("Chave de API carregada com sucesso:", api_key)
+else:
+    print("Chave de API não encontrada no arquivo .env.")
+
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 sample_pdf = genai.upload_file('2402.07927v1.pdf')
 
