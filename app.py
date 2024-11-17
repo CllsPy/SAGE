b21fafde6c44e5aca3301fbe69856d65a560c3a5
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-17 00:04:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ with st.form('Gemini Paper Summarizer'):
                             response = summarize(text_content)
                             summarized_text = (response.text)     
                            
-sub_button = st.form_submit_button('Submit')
+    sub_button = st.form_submit_button('Submit')
+    
 if sub_button:
      st.write(summarized_text)
 
